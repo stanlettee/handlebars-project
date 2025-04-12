176385d879292cccd8f134d949c129bb488915da
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-04-12 16:57:12 +0100

    templates

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import products from "./products.json"
+import makeList from "./templates/product.hbs"
+
+console.log(makeList({products}))
+document.querySelector('div').innerHTML = makeList({products})
