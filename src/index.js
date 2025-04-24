1b908429f5b275935eb2b350414f4ed6850f9b02
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-04-24 18:18:34 +0100

    homework 7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import products from "./products.json"
 import makeList from "./templates/product.hbs"
 
-console.log(makeList({products}))
+// console.log(makeList({products}))
 document.querySelector('div').innerHTML = makeList({products})
